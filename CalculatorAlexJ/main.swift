697c14632925ca085f5960cea76a671ed47b11d3
skrall3119 <alexjanci@yahoo.com>
2020-02-12 12:57:02 -0600

    Initial commit

--- CalculatorAlexJ/main.swift
+++ CalculatorAlexJ/main.swift
@@ -8,5 +8,39 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Enter your first Number")
+var input_1 = readLine()!
+print("Enter the operation")
+var operand = readLine()!
+print("Enter your second Number")
+var input_2 = readLine()!
+
+var firstNumber: Int, secondNumber: Int, operation: String
+
+if let number1 = Int(input_1){
+    firstNumber = number1
+} else {
+    firstNumber = 0
+}
+if let number2 = Int(input_2) {
+    secondNumber = number2
+} else {
+    secondNumber = 0
+}
+
+operation = String(operand)
+
+if(operation == "+"){
+    print("\(firstNumber) \(operation) \(secondNumber) = \(firstNumber + secondNumber)")
+} else if(operation == "-") {
+    print("\(firstNumber) \(operation) \(secondNumber) = \(firstNumber - secondNumber)")
+} else if (operation == "*"){
+    print("\(firstNumber) \(operation) \(secondNumber) = \(firstNumber * secondNumber)")
+} else if (operation == "/"){
+    print("\(firstNumber) \(operation) \(secondNumber) = \(firstNumber / secondNumber)")
+} else if (operation == "%"){
+    print("\(firstNumber) \(operation) \(secondNumber) = \(firstNumber % secondNumber)")
+} else {
+    print("Invalid Operation.")
+}
 
