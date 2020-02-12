c89ee0043337fc5349ae4fb21d8faee471f66265
skrall3119 <alexjanci@yahoo.com>
2020-02-12 12:27:22 -0600

    Initial Commit

---
+++ CalculatorAlexJ/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CalculatorAlexJ
+//
+//  Created by Alex Janci on 2/12/20.
+//  Copyright © 2020 Alex Janci. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
